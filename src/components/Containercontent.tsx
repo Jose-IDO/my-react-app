d0fc721e21764504f947276b9b48b53dd2fa31ef
Joseph IDO <joseph.ido@example.com>
2025-07-28 02:00:56 +0200

    latest updates. nav bar complete

--- src/components/Containercontent.tsx
+++ src/components/Containercontent.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export const Containercontent: React.FC<Props> = ({children, className}) => {
   return (
-    <div className={className} style ={{maxWidth: 1200, padding: 4}} >
+    <div className={className} style ={{width: "100%", padding: 10}} >
         {children}
     </div>
   )
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,63 +1,45 @@
 import React from 'react'
-import { Text} from '../text/text'
+import { Text } from '../text/text'
 import styles from './Navbar.module.css'
 import { Containercontent } from '../Containercontent'
 import mapfinal from '../../assets/map-final.png'
 import cartfinal from '../../assets/cart-final.png'
 
-
 export const Navbar: React.FC<any> = () => {
-    
-    
-return (
-  <nav>
-    <div className={styles.content}>
-      <Containercontent className={styles.content}>
-        
-        <div className={styles.logoContainer}>
-          <img
-            src="https://thumb.ac-illust.com/66/66b1fcb636a43ef47c08c1b0db3a6d14_t.jpeg"
-            alt="company logo"
-            className={styles.logo}
-          />
-          <Text variant="h2" style={{ margin: 0 }}>CodeTribe Shop</Text>
-        </div>
+  return (
+    <nav>
+      <div className={styles.content}>
+        <Containercontent className={styles.content}>
 
-        <div className={styles.links}>
-            <div>
-            <a href="/#" className={styles.link}>Menu</a>
-            <a href="/#" className={styles.link}>Pages</a>
-            <a href="/#" className={styles.link}>Blog</a>
-            <a href="/#" className={styles.link}>Contact Us</a>
-           </div>
+          <div className={styles.logoContainer}>
+            <img
+              src="https://thumb.ac-illust.com/66/66b1fcb636a43ef47c08c1b0db3a6d14_t.jpeg"
+              alt="company logo"
+              className={styles.logo}
+            />
+            <Text variant="h2" style={{ margin: 0 }}>Joux De Vie</Text>
+          </div>
 
-            <div className={styles.buttonsorient}>
-              <button className={styles.buttonCSS}>
-                <img className = {styles.buttonCSS}
-                src= {mapfinal}
-                alt="map icon"
-              />
-              </button>
-              <button className = {styles.buttonCSS}>
-              <img
-              className={styles.buttonCSS}
-                src={cartfinal}
-                alt="cart icon"
-              />
+          <div className={styles.links}>
+            <div className={styles.wordedlinks}>
+              <a href="/#" className={styles.link}>Home</a>
+              <a href="/#" className={styles.link}>Menu</a>
+              <a href="/#" className={styles.link}>Pages</a>
+              <a href="/#" className={styles.link}>Blog</a>
+              <a href="/#" className={styles.link}>Contact Us</a>
+            </div>
+
+            <button className={styles.mapbuttonCSS1}>
+              <img className={styles.mapiconCSS1} src={mapfinal} alt="map icon" />
+            </button>
+
+            <button className={styles.cartbuttonCSS2}>
+              <img className={styles.carticonCSS2} src={cartfinal} alt="cart icon" />
             </button>
           </div>
-        </div>
 
-      </Containercontent>
-    </div>
-  </nav>
-)}
-
-
-
-
-            {/* 
-            <div className={styles['profile-icon']}> 
-              <Text variant="span" style={{ color: 'black' }}>U</Text>
-            </div> 
-            */}
+        </Containercontent>
+      </div>
+    </nav>
+  )
+}
