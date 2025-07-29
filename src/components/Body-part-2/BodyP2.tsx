324bceb3ac9a04dabd76890474ea7e9317c961f9
Joseph IDO <joseph.ido@example.com>
2025-07-29 08:51:56 +0200

    latest updates

--- src/components/Body-part-2/BodyP2.tsx
+++ src/components/Body-part-2/BodyP2.tsx
@@ -5,33 +5,18 @@ import { Containercontent } from '../Containercontent'
 export const BodyP2: React.FC = () => {
   return (
     <Containercontent className={styles.bodyP2}>
-      <div className={styles.innerContent}>
+      <div className={styles.leftside}>
+        <h2 className={styles.juicevscoffee}>Juice Vs Coffee</h2>
+        <p className={styles.subText}>what are the benefits and drawbacks, as a breakfast drink?</p>
+        <button className={styles.learnButton}>Learn More</button>
+      </div>
 
-        <div >
-          <img 
-            src="https://raw.githubusercontent.com/Jose-IDO/my-react-app/main/src/assets/coffeeandjuice.png
-" 
-            alt="Fruit Square" 
-            className={styles.fruitImage} 
-          />
-        </div>
-                <div >
-          <img 
-            src="https://raw.githubusercontent.com/Jose-IDO/my-react-app/main/src/assets/coffeeandjuice.png
-" 
-            alt="Fruit Square" 
-            className={styles.fruitImage} 
-          />
-        </div>
-                <div >
-          <img 
-            src="https://raw.githubusercontent.com/Jose-IDO/my-react-app/main/src/assets/coffeeandjuice.png
-" 
-            alt="Fruit Square" 
-            className={styles.fruitImage} 
-          />
-        </div>
-        
+      <div className={styles.rightImage}>
+        <img 
+          src="https://raw.githubusercontent.com/Jose-IDO/my-react-app/main/src/assets/coffeeandjuice.png"
+          alt="Coffee and Juice"
+          className={styles.fruitImage}
+        />
       </div>
     </Containercontent>
   )
